114fa87ab5e5d25ace8d320c752fc9c565e87c14
Katerina <psyckalla.gmail.com>
2020-10-16 21:29:03 -0400

    callback functions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Main from "./Main";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import Login from "./LoginRegister/login";
 import Register from "./LoginRegister/register";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
     <div className="App">
-      <Route path="/homepage" component={Main} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
+      <Route path="/homepage" component={Main}>
+        {loggedIn === false ? <Redirect to="/login" /> : <Main />}
+      </Route>
+      <Route
+        path="/login"
+        render={(props) => (
+          <Login loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+        )}
+      ></Route>
+      <Route path="/register" component={Register}>
+        {loggedIn ? <Redirect to="/homepage" /> : <Register />}
+      </Route>
     </div>
   );
 }
--- src/LoginRegister/login.js
+++ src/LoginRegister/login.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
-function Login() {
+function Login(props) {
+  const [error, setError] = useState();
+  const history = useHistory();
+
+  const redirectIfLoggedIn = () => {
+    if (props.loggedIn === true) {
+      history.push("/homepage");
+    }
+  };
+
+  useEffect(() => {
+    redirectIfLoggedIn();
+  }, [props.loggedIn]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const User = {
+      email: event.target["email"].value,
+      password: event.target["password"].value,
+    };
+
+    const loginUser = await fetch("http://localhost:8000/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(User),
+    });
+    const userData = await loginUser.json();
+    if (User.password === userData[0].user_password) {
+      props.setLoggedIn(true);
+      console.log(props.loggedIn);
+      history.push("/homepage");
+    } else {
+      setError(<p>Incorrect Email</p>);
+    }
+  };
+
   return (
     <div className="login">
-      <h1>Register</h1>
-      <form action="/login" method="POST">
+      <h1>Login</h1>
+      <form action="/login" method="POST" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">Email</label>
           <input type="email" id="email" name="email" required />
@@ -13,6 +51,7 @@ function Login() {
         <div>
           <label htmlFor="password">Password</label>
           <input type="password" id="password" name="password" required />
+          {error}
         </div>
         <button type="submit">Login</button>
       </form>
